256de6b3b2fd1ffd25ee3da6f6e4c2155a594523
Braydan Newman <newmanbraydan@gmail.com>
2021-08-26 18:19:42 +1000

    Added list of Cumulative Totals CP1404 prac_04 file

---
+++ CP1404/prac_04/total_income.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {str(month)}: "))
+        incomes.append(income)
+
+    report_print(number_of_months, incomes)
+
+
+def report_print(number_of_months, incomes):
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, number_of_months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
