69258336c0969a9916753c34b71862f0862d6425
Braydan Newman <newmanbraydan@gmail.com>
2021-08-26 18:12:06 +1000

    Added Warm-Up CP1404 prac_04 file

---
+++ CP1404/prac_04/lists_warmup.py
@@ -0,0 +1,36 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]
+# 3
+
+# numbers[-1]
+# 2
+
+# numbers[3]
+# 1
+
+# numbers[:-1]
+# [3, 1, 4, 1, 5, 9]
+
+# numbers[3:4]
+# [1]
+
+# 5 in numbers
+# True
+
+# 7 in numbers
+# False
+
+# "3" in numbers
+# True
+
+# numbers + [6, 5, 3]
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = 'ten'
+
+numbers[-1] = 1
+
+numbers[2:]
+
+9 in numbers
