1b6c79bad35d499fde2f96d1f9b790bb4d84e172
Braydan Newman <newmanbraydan@gmail.com>
2021-08-30 01:00:36 +1000

    Added initial start files to CP1404 Assessment_01

---
+++ CP1404/Assessment_01/main.py
@@ -0,0 +1,14 @@
+"""
+Replace the contents of this module docstring with your own details
+Name: Braydan Newman
+Date started: 30/08/2021
+GitHub URL:
+"""
+
+
+def main():
+    print("Travel Tracker 1.0 - by Braydan Newman")
+
+
+if __name__ == '__main__':
+    main()
